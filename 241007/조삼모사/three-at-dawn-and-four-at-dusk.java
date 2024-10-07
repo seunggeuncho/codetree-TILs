b9f321dcd19fe7482aa3285370ecd9b76c7aa5d1
seunggeuncho <87847677+seunggeuncho@users.noreply.github.com>
2024-10-07 19:25:46 +0900

    Title: 조삼모사, Time: 3036ms, Memory: 11MB, Status: Time Limit Exceed - CodeTree

---
+++ 241007/조삼모사/three-at-dawn-and-four-at-dusk.java
@@ -0,0 +1,62 @@
+import java.util.*;
+import java.io.*;
+public class Main {
+    static int[][] map;
+    static int[] first;
+    static int[] second;
+    static boolean[] visited;
+    static int n,ans;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+        map = new int[n][n];
+        ans = Integer.MAX_VALUE;
+        first = new int[n / 2];
+        second = new int[n / 2];
+        visited = new boolean[n];
+        StringTokenizer st = null;
+        for(int i = 0; i < n; i++){
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < n; j++){
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        permutation(0);
+        System.out.println(ans);
+    }
+    private static void permutation(int depth){
+        if(depth == n / 2){
+            //System.out.println(Arrays.toString(visited));
+            int cnt1 = 0;
+            int cnt2 = 0;
+            for(int j = 0; j < n; j++){
+                if(visited[j]) first[cnt1++] = j;
+                else second[cnt2++] = j;
+            }
+            int val = Math.abs(check(first) - check(second));
+            ans = Math.min(ans, val);
+            return;
+        }
+
+        for(int i = depth ; i < n; i++){
+            if(visited[i] == false){
+            visited[i] = true;
+            permutation(depth + 1);
+            visited[i] = false;
+            }
+        }
+    }
+
+    private static int check(int[] tmp){
+        int val = 0;
+        for(int i = 0; i < n / 2 - 1; i++){
+            for(int j = i + 1; j < n / 2;j++){
+                val += map[tmp[i]][tmp[j]];
+                val += map[tmp[j]][tmp[i]];
+            }
+        }
+       // System.out.println(Arrays.toString(tmp));
+        //System.out.println(val);
+        return val;
+    }
+}
